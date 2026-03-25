4afd864d2d0d64eb51c0ea8575f6d511516a6bcc
matsupiee <btq32jh@icloud.com>
2026-03-25 11:37:34 +0900

    packages/apiのテストがなくなったのでpassWithNoTests:trueにした

--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["src/**/*.test.ts"],
+    passWithNoTests: true,
     testTimeout: 15_000,
     fileParallelism: false,
     globalSetup: ["src/test-setup.ts"],
